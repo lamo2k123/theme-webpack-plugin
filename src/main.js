75e7cd60c6747e2d1b08d75a4ff46fe14d146d93
lamo2k123 <lamo2k123@gmail.com>
2016-04-18 16:59:10 +0300

    1.1.0

--- src/main.js
+++ src/main.js
@@ -3,55 +3,63 @@ import { resolve, dirname } from 'path';
 
 class Theme {
 
-    constructor(regexp, themes) {
-        this.regexp = regexp;
-        this.themes = themes;
-
+    constructor(modules, themes) {
         this.apply = this.apply.bind(this);
+        this.theme = this.theme.bind(this, modules, themes);
+        this.normalModuleFactory = this.normalModuleFactory.bind(this);
+    }
+
+    resolve(key, result, callback) {
+        if(!result) {
+            return callback();
+        }
+
+        result[key] = this.theme(result[key], key);
+
+        return callback(null, result);
+    }
+
+    theme(modules, themes, context, key) {
+        let valid = false;
+
+        switch(typeof modules) {
+            case 'string':
+                valid = modules === context;
+                break;
+            case 'object':
+                if(Array.isArray(modules)) {
+                    valid = modules.indexOf(context) !== -1;
+                } else if(!!modules.test) {
+                    valid = modules.test(context);
+                }
+                break;
+        }
+
+        if(valid) {
+            for(let i in themes) {
+                if(themes.hasOwnProperty(i)) {
+                    if(key == 'request') {
+                        let theme   = context.replace(modules, '$1/' + themes[i]),
+                            dir     = resolve('node_modules', theme);
+
+                        if(existsSync(dir)) {
+                            context = theme;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        return context;
+    }
+
+    normalModuleFactory(nmf) {
+        nmf.plugin('before-resolve', this.resolve.bind(this, 'request'));
     }
 
     apply(compiler) {
-        compiler.plugin("normal-module-factory", nmf => {
-            nmf.plugin("before-resolve", (result, callback) => {
-                if(!result) {
-                    return callback();
-                }
-
-                if(this.regexp.test(result.request)) {
-                    for(var i in this.themes) {
-                        if(this.themes[i].hasOwnProperty(i)) {
-                            if(existsSync(resolve('node_modules', result.request, this.themes[i]))) {
-                                result.request = result.request + '/' + this.themes[i];
-                                break;
-                            }
-                        }
-
-                    }
-                }
-
-                return callback(null, result);
-            });
-
-            nmf.plugin("after-resolve", (result, callback) => {
-                if(!result) {
-                    return callback();
-                }
-
-                if(this.regexp.test(result.resource)) {
-                    for(var i in this.themes) {
-                        if(this.themes[i].hasOwnProperty(i)) {
-                            if(existsSync(resolve('node_modules', result.request, this.themes[i]))) {
-                                result.resource = resolve(dirname(result.resource), this.themes[i]);
-                                break;
-                            }
-                        }
-                    }
-
-                }
-
-                return callback(null, result);
-            });
-        });
+        compiler.plugin('normal-module-factory', this.normalModuleFactory);
     }
 
 }
